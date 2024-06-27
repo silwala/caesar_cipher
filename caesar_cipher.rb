e97f56fd0aeaed1211a9a9f48e98e8b7e7913189
Aditya Silwal <aditsilwal@gmail.com>
2024-06-27 03:56:50 -0400

    add wrap functinoality to caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,18 @@
 def caesar_cipher(plaintext, key)
   plaintext_arr = plaintext.split("")
   ciphered_text_arr = plaintext_arr.map do |char|
-    unless (char.ord >= 65 && char.ord <= 90) || (char.ord >= 97 && char.ord <= 122)
-      char
+    if char.ord >= 65 && char.ord <= 90
+      ciphered_char_ord = ((char.ord + key - 64) % 26) + 64
+      ciphered_char_ord.chr
+    elsif char.ord >= 97 && char.ord <= 122
+      ciphered_char_ord = ((char.ord + key - 96) % 26) + 96
+      ciphered_char_ord.chr
     else
-      (char.ord + key).chr
+      char
     end
   end
   ciphered_text_arr.join
 end
 
-puts caesar_cipher("abc123!!!", 5)
+puts caesar_cipher("XYZ!!!", 5)
 
