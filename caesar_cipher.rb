f0fdf101c9fb4c9ed71f9b01b7d53c9106900fb0
Aditya Silwal <aditsilwal@gmail.com>
2024-07-05 02:41:04 -0400

    separate the concerns to different methods  to make each method more concise and make the logic clearer

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(plaintext, key)
-  plaintext_arr = plaintext.split("")
-  ciphered_text_arr = plaintext_arr.map do |char|
-    if char.ord >= 65 && char.ord <= 90
-      ciphered_char_ord = ((char.ord + key - 64) % 26) + 64
-      ciphered_char_ord.chr
-    elsif char.ord >= 97 && char.ord <= 122
-      ciphered_char_ord = ((char.ord + key - 96) % 26) + 96
-      ciphered_char_ord.chr
-    else
-      char
-    end
-  end
-  ciphered_text_arr.join
+  plaintext.split('').map { |char| cipher_char(char, key) }.join
 end
 
-puts caesar_cipher("XYZ!!!", 5)
+def cipher_char(char, key)
+  if char.ord.between?(65, 90)
+    (((char.ord + key - 64) % 26) + 64).chr
+  elsif char.ord.between?(97, 122)
+    (((char.ord + key - 96) % 26) + 96).chr
+  else
+    char
+  end
+end
+
+puts caesar_cipher('XYZ!!!', 31)
 
